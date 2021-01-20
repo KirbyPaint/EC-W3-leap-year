7768e372dd94196be3b786fb8764f9ccf29a52f5
Ashley Porter <porter.ashley13@gmail.com>
2021-01-20 11:45:33 -0800

    add code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
 
+  // The year is evenly divisible by 4; DONE
+  // If the year can be evenly divided by 100, it is NOT a leap year, unless;
+  // The year is also evenly divisible by 400. Then it is a leap year.
+
+$(document).ready(function() {
+  // event.preventDefault();
+  $("button#start").click(function() {
+    const inputValue = parseInt($("#formResponse").val());
+    
+    if ((inputValue % 4) === 0) {
+      if ((inputValue % 100) === 0) {
+        if ((inputValue % 400) === 0) {
+          alert("Leap year");
+        }
+        else {
+          alert("Not a leap year");
+        }
+      }
+      else {
+        alert("Leap year");
+      }
+    }
+    else {
+      alert("Not a leap year");
+    }
+  });
 });
